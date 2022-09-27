3053b49f18a95b770732bee9280da7c87757b6f4
RejownAhmed <rejownahmedzisan@gmail.com>
2022-09-27 21:54:09 +0600

    showing loader functionality added

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,8 @@
     <ul>
       <li class="list-item" v-for="item in items" :key="item.id">{{item.name}}</li>
     </ul>
-    <Observer @intersect="intersected"/>
+    <h1 v-show="loading" class="loading">Loading....</h1><!--Loader-->
+    <Observer @intersect="fetchData"/>
   </div>
 </template>
 
@@ -13,11 +14,31 @@ import Observer from "./components/Observer.vue"
 
 const page = ref(1)
 const items = ref([])
-const intersected = async () => {
+const loading = ref(false)
+//Just for demo purposes to show the loader working
+const delay = (ms) => new Promise(resolve => setTimeout(resolve, ms)) //DEMO PURPOS ONLY
+const fetchData = async () => {
+  try {
+    loading.value = true// Show loader
     const res = await fetch(`https://jsonplaceholder.typicode.com/comments?_page=${page.value}&_limit=50`);
-
-    page.value++;
     const newItems = await res.json();
+    await delay(2000)
     items.value = [...items.value, ...newItems];
+    loading.value = false //As all promise completed hide the loader
+    return
+  } catch(error){
+    console.log("Erro Occured:" + error)
   }
+}
+
 </script>
+
+<style>
+
+.loading{
+  width: 100%;
+  background-color: red;
+  text-align: center;
+  color: white
+}
+</style>
